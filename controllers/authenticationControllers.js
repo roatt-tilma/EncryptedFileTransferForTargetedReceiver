357ac911fb1b888d82f8c7ae3f1e766c0820c7a8
umsisamess <umeshsaud2001@gmail.com>
2022-04-06 23:01:07 +0530

    email and password hashing

---
+++ controllers/authenticationControllers.js
@@ -0,0 +1,46 @@
+const User = require('../models/user.model');
+const bcrypt = require('bcrypt');
+
+const user_signup = (req,res) => {
+    const userEmail = req.body.email;
+    const password = req.body.password;
+    const confPassword = req.body.password_conf;
+
+
+    User.find({email : userEmail},async (err,data) => {
+        if(err){
+            console.log('Error occured..')
+            res.status(400).json('Error occured: ' + err);
+        }
+        else{
+            if(data.length == 0){
+                if(password == confPassword){
+                    const salt = await bcrypt.genSalt()
+                    const hashedEmail = await bcrypt.hash(userEmail,salt)
+                    const hashedPassword = await bcrypt.hash(password,salt)
+                    console.log(salt)
+                    console.log(hashedEmail)
+                    console.log(hashedPassword)
+                    const newUser = new User({
+                        email : hashedEmail,
+                        password : hashedPassword,
+                    });
+                    newUser.save()
+                    .then(() => {
+                        res.redirect('/');
+                    })
+                    .catch((err)=>{
+                        res.status(400).json('Error: ' + err);
+                    })
+                }
+            }
+            else{
+                console.log("Given email is already in use");
+                res.json('Error : Given email is already in use');
+            }
+        }
+    })
+
+}
+
+module.exports = user_signup;
