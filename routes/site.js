4bfd1c9e213b707fd3b554623c39f7b5b24b2930
roatt-tilma <roshanbhattmnr@gmail.com>
2022-04-14 16:48:21 +0530

    some UI

--- routes/site.js
+++ routes/site.js
@@ -4,4 +4,8 @@ router.route('/upload').get((req,res) => {
     res.render('upload', { title: 'UPLOAD' })
 });
 
+router.route('/receiveremail').get((req, res) => {
+    res.render('receiveremailform', { title: 'RECEIVER Email' });
+})
+
 module.exports = router;
